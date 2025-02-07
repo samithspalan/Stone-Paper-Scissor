f0ff99261f3235f1834362b639152e521e19e499
samithspalan <samithpoojary12345@gmail.com>
2025-02-07 19:22:26 +0530

    first commit

--- script.js
+++ script.js
@@ -1,27 +1,67 @@
 let choices=document.querySelectorAll(".choice");
 let you=document.querySelector(".you");
 let AI=document.querySelector(".AI");
+let result=document.querySelector(".result");
 choices.forEach((choice)=>{
     choice.addEventListener('click',()=>{
-       let userChoice=choice.getAttribute("id");
+        result.innerText="";
+        result.style="box-shadow: inset 2px 2px 20px rgba(57, 57, 57, 0.8),inset -2px -2px 20px rgba(51, 51, 51, 0.8);";
+        startshake();
+       setTimeout(()=>{
+        stopshake();
+        let userChoice=choice.getAttribute("id");
        let options=["stone","paper","scissor"];
        let compindex=Math.floor(Math.random()*3);
        let compChoice=options[compindex];
        playgame(userChoice,compChoice);
-    })
+       },1000);
+    });
    
 });
+function startshake(){
+    you.style="animation: shake 1s linear 1;"
+    AI.style="animation: shake 1s linear 1;"
+}
+function stopshake(){
+    you.style="animation:none;"
+    AI.style="animation: none;"
+}
 function playgame(userChoice,compChoice){
+    if(userChoice==="stone"){
+        you.innerText="👊"
+    }else if(userChoice==="paper"){ 
+        you.innerText="🤚"
+    }else if(userChoice==="scissor"){ 
+        you.innerText="✌️"
+    }
+    if(compChoice==="stone"){
+        AI.innerText="👊"
+    }else if(compChoice==="paper"){ 
+        AI.innerText="🤚"
+    }else if(compChoice==="scissor"){ 
+        AI.innerText="✌️"
+    }
+
     if(userChoice===compChoice){
-        console.log("draw");
+        result.innerText="DRAW";
+        result.style=" box-shadow: inset 2px 2px 20px rgba(245, 251, 50, 0.8),inset -2px -2px 20px rgba(234, 240, 65, 0.8);";
+        result.style.color="rgba(243, 184, 56, 0.8)";
     }else if(userChoice==="stone" && compChoice==="scissor"){
-        console.log("you won");
+        result.innerText="YOU WON💪";
+        result.style=" box-shadow: inset 2px 2px 20px rgba(9, 251, 9, 0.8),inset -2px -2px 20px rgba(109, 245, 5, 0.8);";
+        result.style.color="rgba(26, 161, 11, 0.8)";
     }else if(userChoice==="paper" && compChoice==="stone"){
-        console.log("you won");
+        result.innerText="YOU WON💪";
+        result.style=" box-shadow: inset 2px 2px 20px rgba(9, 251, 9, 0.8),inset -2px -2px 20px rgba(109, 245, 5, 0.8);";
+        result.style.color="rgba(26, 161, 11, 0.8)";
     }else if(userChoice==="scissor" && compChoice==="paper"){
-        console.log("you won");
+        result.innerText="YOU WON💪";
+        result.style=" box-shadow: inset 2px 2px 20px rgba(9, 251, 9, 0.8),inset -2px -2px 20px rgba(109, 245, 5, 0.8);";
+        result.style.color="rgba(26, 161, 11, 0.8)";
     }else{
-        console.log("you loose")
+        result.innerText="LOST😒";  
+         result.style=" box-shadow: inset 2px 2px 20px rgba(251, 126, 9, 0.8),inset -2px -2px 20px rgba(252, 61, 18, 0.8);";
+        result.style.color="rgba(245, 45, 10, 0.8)";
     }
 
 }
